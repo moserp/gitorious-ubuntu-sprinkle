281c23c0a80ef346a0cef01ee05f9e05d15d5c59
Alkesh Vaghmaria <alkesh.vaghmaria@bt.com>
2010-11-03 10:40:52 +0000

    various fixes

--- gitorious.rb
+++ gitorious.rb
@@ -8,12 +8,13 @@ policy :gitorious, :roles => :server do
   requires :initscripts
   requires :git_user
   requires :git_directories
+  requires :start_daemons
 end
 
 package :initscripts do
   requires :gitdaemon_initd
-  requires :gitpoller_initd
   requires :stomp_initd
+  requires :gitpoller_initd
 end
 
 package :config do
@@ -40,7 +41,7 @@ end
 
 package :gitorious_dependencies do
   gems = [:chronic, :daemons, :hoe, :echoe, :'ruby-yadis', :'ruby-openid',
-    :'mime-types', :'diff-lcs', :json, :'ruby-hmac', :stompserver]
+    :'mime-types', :'diff-lcs', :json, :'ruby-hmac', :stompserver, :mysql]
 
   gems.each do |gem_name|
     puts "installing #{gem_name}"
@@ -71,8 +72,9 @@ package :gitorious do
   requires :gitorious_dependencies
   gem 'mysql' do
     http_proxy 'http://proxy.intra.bt.com:8080'
-    pre :install, 'bash -c "export http_proxy=http://proxy.intra.bt.com:8080 && wget http://gitorious.org/gitorious/mainline/archive-tarball/master -O gitorious.tar.gz"'
-    pre :install, 'tar xfz ~/gitorious.tar.gz -C /var/www && mv /var/www/gitorious-mainline /var/www/gitorious'
+    pre :install, 'bash -c "export http_proxy=http://proxy.intra.bt.com:8080 && wget http://gitorious.org/gitorious/mainline/archive-tarball/master -O /tmp/gitorious.tar.gz"'
+    pre :install, 'tar xfz ~/gitorious.tar.gz -C /var/www'
+    pre :install, 'mv /var/www/gitorious-mainline /var/www/gitorious'
     pre :install, 'adduser --system --home /var/www/gitorious/ --no-create-home --group --shell /bin/bash git chown -R git:git /var/www/gitorious'
     pre :install, 'chown -R git:git /var/www/gitorious'
     # this next line should work, but http git clone is flakey on gitorious, so using the above tar file instead
@@ -85,40 +87,73 @@ package :gitorious do
   end
 end
 
-package :gitpoller_initd do
-  transfer 'init.d/git-poller', 'git-poller' do
-    post :install, 'mv git-poller /etc/init.d/git-poller && chmod 755 /etc/init.d/git-poller'
-    post :install, 'update-rc.d git-poller defaults'
-    post :install, '/etc/init.d/git-poller start'
-  end
-  verify do
-    has_file '/etc/init.d/git-poller'
-  end
-end
-
 package :stomp_initd do
   transfer 'init.d/stomp', 'stomp' do
-    post :install, 'mv stomp /etc/init.d/stomp && chmod 755 /etc/init.d/stomp'
-    post :install, 'update-rc.d stomp defaults'
-    post :install, '/etc/init.d/stomp start'
+    post :install, 'mv stomp /etc/init.d/stomp'
   end
   verify do
     has_file '/etc/init.d/stomp'
   end
 end
 
+package :gitpoller_initd do
+  transfer 'init.d/git-poller', 'git-poller' do
+    post :install, 'mv git-poller /etc/init.d/git-poller'
+  end
+  verify do
+    has_file '/etc/init.d/git-poller'
+  end
+end
+
 package :gitdaemon_initd do
-  noop do
-    post :install, 'cp ~git/doc/templates/ubuntu/git-daemon /etc/init.d/git-daemon'
-    post :install, 'update-rc.d git-daemon defaults'
-    post :install, '/etc/init.d/git-daemon start'
+  transfer 'init.d/git-daemon', 'git-daemon' do
+    post :install, 'mv git-daemon /etc/init.d/git-daemon'
   end
   verify do
     has_file '/etc/init.d/git-daemon'
   end
 end
 
+package :gitorious_database do
+  noop do
+    post :install, 'su - git -c "rake db:create db:migrate RAILS_ENV=production"'
+  end
+end
+
+package :start_stomp do
+  noop do
+    post :install, 'chown root:root /etc/init.d/stomp'
+    post :install, 'chmod 755 /etc/init.d/stomp'
+    post :install, 'update-rc.d stomp defaults'
+    post :install, '/etc/init.d/stomp start'
+  end
+  verify {has_process 'stompserver'}
+end
+
+package :start_ruby_daemons do
+  noop do
+    post :install, 'chown root:root /etc/init.d/git-poller'
+    post :install, 'chmod 755 /etc/init.d/git-poller'
+    post :install, 'update-rc.d git-poller defaults'
+    post :install, '/etc/init.d/git-poller start'
+    post :intsall, 'chown root:root /etc/init.d/git-daemon'
+    post :install, 'update-rc.d git-daemon defaults'
+    post :install, '/etc/init.d/git-daemon start'
+  end
+  verify {has_process 'ruby'}
+end
+
+package :start_daemons do
+  requires :stomp_initd
+  requires :gitpoller_initd
+  requires :gitdaemon_initd
+  requires :gitorious_database
+  requires :start_stomp
+  requires :start_ruby_daemons
+end
+
 package :database_config do
+  requires :git_user
   transfer 'config/database.yml', '/tmp/database.yml' do
     post :install, 'mv /tmp/database.yml ~git/config/database.yml'
     post :install, 'chown git:git ~git/config/database.yml'
@@ -127,6 +162,7 @@ package :database_config do
 end
 
 package :gitorious_config do
+  requires :git_user
   transfer 'config/gitorious.yml', '/tmp/gitorious.yml' do
     post :install, 'mv /tmp/gitorious.yml ~git/config/gitorious.yml'
     post :install, 'chown git:git ~git/config/gitorious.yml'
@@ -143,21 +179,19 @@ end
 
 package :git_user do
   noop do
-    pre :install, 'adduser --system --home /var/www/gitorious/ --no-create-home --group --shell /bin/bash git'
+    post :install, 'adduser --system --home /var/www/gitorious/ --no-create-home --group --shell /bin/bash git'
     post :install, 'chown -R git:git /var/www/gitorious'
   end
 
-  verify do
-    file_contains '/etc/passwd', 'git'
-  end
+  verify {file_contains '/etc/passwd', 'git'}
 end
 
 package :git_directories do
   noop do
     post :install, "su git -c 'mkdir ~git/.ssh'"
     post :install, "su git -c 'touch ~git/.ssh/authorized_keys'"
-    post :install, "su git -c 'chmod 700 ~git/.ssh'"
-    post :install, "su git -c 'chmod 600 ~git/ssh/authorized_keys'"
+    post :install, "chmod 700 ~git/.ssh"
+    post :install, "chmod 600 ~git/.ssh/authorized_keys"
     post :install, "su git -c 'mkdir ~git/tmp/pids'"
     post :install, "su git -c 'mkdir ~git/repositories'"
     post :install, "su git -c 'mkdir ~git/tarballs'"
